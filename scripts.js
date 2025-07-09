a618dd74f44b1af682b7b0190d2333d46b57d3ed
Kim <76394439+kimpsp@users.noreply.github.com>
2025-07-10 04:11:53 +0700

    Update scripts.js
    тест json формата

--- scripts.js
+++ scripts.js
@@ -1,15 +1,53 @@
-<script>
-  function selectQuest(button) {
-    // Находим все карточки
-    const cards = document.querySelectorAll('.quest-card');
-
-    // Убираем у всех класс 'active'
-    cards.forEach(card => card.classList.remove('active'));
-
-    // Находим родителя кнопки (это наш .quest-card)
-    const selectedCard = button.closest('.quest-card');
-
-    // Добавляем класс 'active' только этой карточке
-    selectedCard.classList.add('active');
+const quests = [
+  {
+    id: 1,
+    title: "Лабиринт",
+    status: "Переработка",
+    available: true,
+    file: "quest1.html"
+  },
+  {
+    id: 2,
+    title: "Лабиринт v2",
+    status: "Доступен",
+    available: true,
+    file: "quest2.html"
+  },
+  {
+    id: 3,
+    title: "Лабиринт v3",
+    status: "На испытаниях",
+    available: true,
+    file: "quest3.html"
+  },
+  {
+    id: 4,
+    title: "Проклятие болот",
+    status: "В разработке",
+    available: false
+  },
+  {
+    id: 5,
+    title: "Башня теней",
+    status: "В разработке",
+    available: false
   }
-</script>
+];
+
+
+
+// <script>
+//   function selectQuest(button) {
+//     // Находим все карточки
+//     const cards = document.querySelectorAll('.quest-card');
+
+//     // Убираем у всех класс 'active'
+//     cards.forEach(card => card.classList.remove('active'));
+
+//     // Находим родителя кнопки (это наш .quest-card)
+//     const selectedCard = button.closest('.quest-card');
+
+//     // Добавляем класс 'active' только этой карточке
+//     selectedCard.classList.add('active');
+//   }a
+// </script>
