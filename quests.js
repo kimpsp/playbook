a82ed558df1ee524856229554ff7d7175d7e2e57
Kim <76394439+kimpsp@users.noreply.github.com>
2025-07-10 15:51:21 +0700

    Update quests.js

--- quests.js
+++ quests.js
@@ -40,21 +40,3 @@ const quests = [
     available: false
   }
 ];
-
-
-
-// <script>
-//   function selectQuest(button) {
-//     // Находим все карточки
-//     const cards = document.querySelectorAll('.quest-card');
-
-//     // Убираем у всех класс 'active'
-//     cards.forEach(card => card.classList.remove('active'));
-
-//     // Находим родителя кнопки (это наш .quest-card)
-//     const selectedCard = button.closest('.quest-card');
-
-//     // Добавляем класс 'active' только этой карточке
-//     selectedCard.classList.add('active');
-//   }a
-// </script>
